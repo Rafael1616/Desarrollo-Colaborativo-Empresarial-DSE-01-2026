f7cafdc2c659de4c3a4e6d012c6e26ff7a4a328f
Rafael1616 <sd0058032023@unab.edu.sv>
2026-02-28 14:53:01 -0600

    func: la creacion de un array

---
+++ app.js
@@ -0,0 +1,38 @@
+// Array donde guardamos los productos
+let productos = [
+    {
+        nombre: "Laptop Lenovo IdeaPad 3",
+        descripcion: "Laptop 15.6\" Ryzen 5, 8GB RAM, 512GB SSD",
+        precio: 650,
+        stock: 10,
+        estado: "Disponible"
+    },
+    {
+        nombre: "Mouse Logitech G203",
+        descripcion: "Mouse gamer RGB 8000 DPI",
+        precio: 25,
+        stock: 30,
+        estado: "Disponible"
+    },
+    {
+        nombre: "Teclado Mecánico Redragon K552",
+        descripcion: "Teclado mecánico switches azules",
+        precio: 45,
+        stock: 15,
+        estado: "Disponible"
+    },
+    {
+        nombre: "Monitor Samsung 24\"",
+        descripcion: "Monitor Full HD 75Hz HDMI",
+        precio: 140,
+        stock: 8,
+        estado: "Agotado"
+    },
+    {
+        nombre: "Disco SSD Kingston 1TB",
+        descripcion: "SSD SATA 2.5\" 1TB",
+        precio: 80,
+        stock: 20,
+        estado: "Disponible"
+    }
+];
