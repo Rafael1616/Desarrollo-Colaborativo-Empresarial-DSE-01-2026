44855f2800b827a354f86db124e5b5640a8a4bdb
Marlon22ffa <ma0565032022@unab.edu.sv>
2026-03-03 09:10:38 -0600

    func: alerta al editar producto

--- app.js
+++ app.js
@@ -156,3 +156,23 @@ function mostrarProductos() {
 // ===============================
 // Se ejecuta al cargar la página para mostrar los productos iniciales.
 mostrarProductos();
+
+// Carga los datos del producto seleccionado al formulario
+function editarProducto(index) {
+    const producto = productos[index];
+
+    document.getElementById("nombre").value = producto.nombre;
+    document.getElementById("descripcion").value = producto.descripcion;
+    document.getElementById("precio").value = producto.precio;
+    document.getElementById("stock").value = producto.stock;
+    document.getElementById("estado").value = producto.estado;
+
+    indiceInput.value = index;
+}
+
+// Actualiza el producto en el array con los nuevos datos
+function actualizarProducto() {
+    const index = Number(indiceInput.value);
+    productos[index] = obtenerDatosFormulario();
+	alert("✅ Producto actualizado correctamente.")
+}
